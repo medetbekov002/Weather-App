f228fdac178dc47a03c878200bb392977cef91f8
medetbekov002 <valvekakaxa@gmail.com>
2024-04-26 18:38:26 +0600

    Start (5%)

---
+++ app/src/main/java/com/example/weatherapp/ui/fragments/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.weatherapp.ui.fragments.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
