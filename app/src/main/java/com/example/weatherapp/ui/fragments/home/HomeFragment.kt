715d550a93be5daa6dfc41323b74064c2333508d
medetbekov002 <valvekakaxa@gmail.com>
2024-04-28 14:48:51 +0600

    30%

--- app/src/main/java/com/example/weatherapp/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/fragments/home/HomeFragment.kt
@@ -12,6 +12,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.weatherapp.R
 import com.example.weatherapp.data.CurrentLocation
 import com.example.weatherapp.databinding.FragmentHomeBinding
 import com.example.weatherapp.storage.SharedPreferencesManager
@@ -126,6 +128,7 @@ class HomeFragment : Fragment() {
             setItems(options) { _, which ->
                 when (which) {
                     0 -> proceedWithCurrentLocation()
+                    1 -> startManualLocationSearch()
                 }
             }
             show()
@@ -144,6 +147,10 @@ class HomeFragment : Fragment() {
             weatherDataRecyclerView.visibility = View.VISIBLE
             swipeRefreshLayout.isRefreshing = false
         }
+    }
+
+    private fun startManualLocationSearch() {
+        findNavController().navigate(R.id.action_home_fragment_to_location_fragment)
     }
 
 }
--- app/src/main/java/com/example/weatherapp/ui/fragments/location/LocationFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/fragments/location/LocationFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentLocationBinding
 
 class LocationFragment : Fragment() {
+
+    private var _binding: FragmentLocationBinding? = null
+    private val binding get() = requireNotNull(_binding)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        _binding = FragmentLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.ivClose.setOnClickListener { findNavController().popBackStack() }
     }
 
 }
