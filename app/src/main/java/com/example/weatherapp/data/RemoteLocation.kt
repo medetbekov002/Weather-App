a3779e5e32b04dc6a954183855001f23ae661ad5
medetbekov002 <valvekakaxa@gmail.com>
2024-04-29 12:46:33 +0600

    35%

---
+++ app/src/main/java/com/example/weatherapp/data/RemoteLocation.kt
@@ -0,0 +1,1 @@
+package com.example.weatherapp.dataclass RemoteLocation (    val name: String,    val region: String,    val country: String,    val lat: Double,    val lon: Double,//    val tz_id: String,//    val localtime_epoch: Long,)
